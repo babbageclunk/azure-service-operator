a34181c1ca1a522abb44da501ddc3def11a981ec
Christian Muirhead <xtian@babbageclunk.com>
2021-07-05 11:16:21 +1200

    Add a sweeper to remove DBs that haven't been used recently

--- pkg/helpers/dbcache.go
+++ pkg/helpers/dbcache.go
@@ -8,21 +8,42 @@ import (
 	"sync"
 	"time"
 
+	"github.com/hashicorp/go-multierror"
 	"github.com/pkg/errors"
 )
 
 const (
 	// maxIdleTimeSeconds is how long we'll keep an idle connection
 	// open in the pool.
-	maxIdleTimeSeconds = 5 * 60
+	maxConnIdleTimeSeconds = 5 * 60
+
+	// dbRemovalSeconds is how long we'll wait after a DB is last used
+	// to remove it from the cache.
+	dbRemovalSeconds = 20 * 60
+
+	// sweepIntervalSeconds is how long we wait between sweeps.
+	sweepIntervalSeconds = 60
+
+	// sweeperStopTimeoutSeconds is how long we'll wait for the
+	// sweeper goroutine to stop before complaining.
+	sweeperStopTimeoutSeconds = 5
 )
 
 // NewDBCache returns a new empty cache of DB connections.
 func NewDBCache(driverName string) *DBCache {
-	return &DBCache{
+	cache := DBCache{
 		driverName: driverName,
-		items:      make(map[string]*sql.DB),
+		items:      make(map[string]*dbEntry),
+		stop:       make(chan struct{}),
+		stopped:    make(chan struct{}),
 	}
+	go cache.runSweeper()
+	return &cache
+}
+
+type dbEntry struct {
+	db       *sql.DB
+	lastUsed time.Time
 }
 
 // DbCache keeps a cache of existing *sql.DBs for a specific driver
@@ -31,7 +52,9 @@ func NewDBCache(driverName string) *DBCache {
 type DBCache struct {
 	driverName string
 	mutex      sync.Mutex
-	items      map[string]*sql.DB
+	items      map[string]*dbEntry
+	stop       chan struct{}
+	stopped    chan struct{}
 }
 
 // Get returns an existing DB for sourceName if one exists. If not it
@@ -41,20 +64,73 @@ func (c *DBCache) Get(sourceName string) (*sql.DB, error) {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
 
-	db, found := c.items[sourceName]
+	entry, found := c.items[sourceName]
 	if !found {
-		newDb, err := sql.Open(c.driverName, sourceName)
+		newDB, err := sql.Open(c.driverName, sourceName)
 		if err != nil {
 			return nil, errors.Wrap(err, "opening new DB")
 		}
-		db = newDb
-		c.items[sourceName] = newDb
-		db.SetConnMaxIdleTime(time.Second * maxIdleTimeSeconds)
+		newDB.SetConnMaxIdleTime(maxConnIdleTimeSeconds * time.Second)
+		entry = &dbEntry{
+			db:       newDB,
+			lastUsed: time.Now(),
+		}
+		c.items[sourceName] = entry
+	} else {
+		entry.lastUsed = time.Now()
 	}
-	return db, nil
+	return entry.db, nil
 }
 
-// TODO: provide a way to remove from the cache when we delete a
-// database. Without this, when a database is removed the connections
-// will eventually be removed once the idle timeout expires, but the
-// cached sql.DB won't ever be removed from the map.
+func (c *DBCache) runSweeper() {
+	defer close(c.stopped)
+	ticker := time.NewTicker(sweepIntervalSeconds * time.Second)
+	for {
+		select {
+		case <-c.stop:
+			return
+		case <-ticker.C:
+			c.sweep()
+		}
+	}
+}
+
+func (c *DBCache) sweep() {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	now := time.Now()
+	for key, entry := range c.items {
+		removeTime := entry.lastUsed.Add(dbRemovalSeconds * time.Second)
+		if now.Before(removeTime) {
+			continue
+		}
+		delete(c.items, key)
+		// TODO: logging here?
+		entry.db.Close()
+	}
+}
+
+func (c *DBCache) stopSweeper() error {
+	close(c.stop)
+	select {
+	case <-time.After(sweeperStopTimeoutSeconds * time.Second):
+		return errors.Errorf("timed out waiting for sweeper to stop")
+	case <-c.stopped:
+		return nil
+	}
+}
+
+func (c *DBCache) Close() error {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	var errs *multierror.Error
+	for _, entry := range c.items {
+		errs = multierror.Append(errs, entry.db.Close())
+	}
+
+	// Stop the sweeper goroutine and wait for it to finish.
+	errs = multierror.Append(errs, c.stopSweeper())
+	return errs.ErrorOrNil()
+}
